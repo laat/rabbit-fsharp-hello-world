3a1da7a6b08ac4a69b169e5f28988686ca0fcf3c
Sigurd Fosseng <sigurd@fosseng.net>
2020-01-17 21:08:10 +0100

    init with rabbitmqadmin

--- scripts/rabbit-init.sh
+++ scripts/rabbit-init.sh
@@ -5,4 +5,12 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-curl -u guest:guest -X PUT http://localhost:15672/api/vhosts/console-test
+# change workdir relative to this scirpt
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+curl http://localhost:15672/cli/rabbitmqadmin -o "rabbitmqadmin"
+chmod +x "rabbitmqadmin"
+
+./rabbitmqadmin declare vhost name=console-test
+
+# curl -u guest:guest -X PUT http://localhost:15672/api/vhosts/console-test
